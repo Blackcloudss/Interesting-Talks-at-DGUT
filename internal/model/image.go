e8a78c22a3c8657b9d8e981bbe4fffc831dc3904
Blackcloudss <3444535358@qq.com>
2025-03-17 17:40:55 +0800

    feat:增加图片保存功能

---
+++ internal/model/image.go
@@ -0,0 +1,13 @@
+package model
+
+// @Title        image.go
+// @Description
+// @Create       XdpCs 2025-03-17 上午2:21
+// @Update       XdpCs 2025-03-17 上午2:21
+
+type Image struct {
+	CommonModel
+	ImagePath string `gorm:"column:image_path;type:varchar(255);comment:'图片路径'"`
+	Size      int64  `gorm:"column:size;type:bigint;comment:'文件大小'"`
+	BlogID    int64  `gorm:"column:blog_id;type:bigint;comment:'博客ID'"`
+}
---
+++ utils/image/image.go
@@ -0,0 +1,95 @@
+package image
+
+import (
+	"fmt"
+	"github.com/Blackcloudss/Interesting-Talks-at-DGUT/global"
+	"github.com/Blackcloudss/Interesting-Talks-at-DGUT/log/zlog"
+	"github.com/Blackcloudss/Interesting-Talks-at-DGUT/utils/snowflake"
+	"io"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+// @Title        image.go
+// @Description
+// @Create       XdpCs 2025-03-17 下午3:09
+// @Update       XdpCs 2025-03-17 下午3:09
+func UploadImage(file *multipart.FileHeader) (ImageUrl string, err error) {
+	//图片大小限制
+	if file.Size > global.IMAGE_SIZE {
+		zlog.Errorf("图片大小超过限制")
+		return
+	}
+	//图片格式限制
+	filename := file.Filename
+	ext, exist := ImageExtJudge(filename)
+	if !exist {
+		zlog.Errorf("图片格式不正确")
+		return
+	}
+	// 生成唯一文件名
+	newFilename := snowflake.GetString12Id(global.Node) + ext
+
+	// 按日期分目录存储
+	dateDir := time.Now().Format("2006-01") // 格式如 "2023-10"
+	saveDir := filepath.Join(global.IMAGE_PATH, dateDir)
+	savePath := filepath.Join(saveDir, newFilename)
+
+	// 创建目录
+	if err = os.MkdirAll(saveDir, os.ModePerm); err != nil {
+		zlog.Errorf("创建目录失败:%v", err)
+		return
+	}
+
+	// 保存文件到本地
+	if err = SaveUploadFile(file, savePath); err != nil {
+		zlog.Errorf("保存文件失败:%v", err)
+		return "", err
+	}
+
+	// 返回相对路径（如 /images/2023-10/雪花id.jpg）
+	ImageUrl = fmt.Sprintf("/%s/%s/%s", global.IMAGE_PATH, dateDir, newFilename)
+	return
+}
+
+// 校验文件类型和大小
+var whiteList = []string{".jpg", ".png", ".jpeg", ".gif", ".webp"}
+
+func ImageExtJudge(FileName string) (ext string, exist bool) {
+	ext = filepath.Ext(FileName)
+	for _, v := range whiteList {
+		if strings.ToLower(ext) == v {
+			return ext, true
+		}
+	}
+	return "", false
+}
+
+// 保存文件到本地
+func SaveUploadFile(file *multipart.FileHeader, savePath string) error {
+	// 打开文件
+	src, err := file.Open()
+	if err != nil {
+		zlog.Errorf("打开文件失败:%v", err)
+		return err
+	}
+	defer src.Close()
+
+	// 创建本地文件
+	out, err := os.Create(savePath)
+	if err != nil {
+		zlog.Errorf("创建本地文件失败:%v", err)
+		return err
+	}
+	defer out.Close()
+
+	// 将文件内容写入到本地文件
+	if _, err = io.Copy(out, src); err != nil {
+		zlog.Errorf("写入本地文件失败:%v", err)
+		return err
+	}
+	return nil
+}
