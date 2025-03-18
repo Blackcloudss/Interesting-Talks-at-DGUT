d04f05f7f78e767c01b379c6bb446305b55b0163
Blackcloudss <3444535358@qq.com>
2025-03-18 09:58:40 +0800

    fix:先注释init_filter相关代码,确保项目能部署到服务器

--- initalize/init_filter.go
+++ initalize/init_filter.go
@@ -1,16 +1,11 @@
 package initalize
 
-import (
-	"github.com/Blackcloudss/Interesting-Talks-at-DGUT/global"
-	"github.com/Blackcloudss/Interesting-Talks-at-DGUT/log/zlog"
-)
-
 func InitFilter() {
 	// 加载本地敏感词字典
-	err := global.SensitiveFilter.LoadWordDict("utils/filter_swords/dict.txt")
-	if err != nil {
-		zlog.Errorf("Failed to load word dict: %v", err)
-	}
+	//err := global.SensitiveFilter.LoadWordDict("utils/filter_swords/dict.txt")
+	//if err != nil {
+	//	zlog.Errorf("Failed to load word dict: %v", err)
+	//}
 	// 也可以加载网络敏感词字典
 	// err = global.SensitiveFilter.LoadNetWordDict(" ")
 	// if err != nil {
