535052dfe9a7d9d579150477f3874cb199e22180
Tz <517719033@qq.com>
2025-03-18 21:58:37 +0800

    fix:删除重复部分

--- internal/model/tz_user.go
+++ internal/model/tz_user.go
@@ -2,16 +2,12 @@ package model
 
 type User struct {
 	CommonModel
-	Username  string `gorm:"unique;not null" json:"username"`          // 用户名唯一且不能为空
-	Role      string `gorm:"default:未授权" json:"role"`                  // 默认角色为学生
-	Nickname  string `gorm:"default:游客" json:"nickname"`               // 默认昵称为“游客”
-	Avatar    string `gorm:"default:default_avatar.png" json:"avatar"` // 默认头像
 	Followers []User `gorm:"many2many:user_follows;joinForeignKey:ID;joinReferences:FollowedID" json:"followers"`
 	Following []User `gorm:"many2many:user_follows;joinForeignKey:ID;joinReferences:FollowerID" json:"following"`
 }
 
 type Follow struct {
 	CommonModel
-	FollowerID int64 `gorm:"primaryKey;autoIncrement:false" json:"follower_id"` // 关注者ID
-	FollowedID int64 `gorm:"primaryKey;autoIncrement:false" json:"followed_id"` // 被关注者ID
+	FollowerID int64 `gorm:"index" json:"follower_id"` // 关注者ID
+	FollowedID int64 `gorm:"index" json:"followed_id"` // 被关注者ID
 }
