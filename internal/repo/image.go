560387c693f6b0ba7ea685d194b07cb43b6357d7
Tz <517719033@qq.com>
2025-03-18 15:21:03 +0800

    feat:获取图片信息

--- internal/repo/image.go
+++ internal/repo/image.go
@@ -18,6 +18,15 @@ func (r *ImageRepo) CreateImage(image *model.Image) error {
 	return r.DB.Create(image).Error
 }
 
-//删除图片
+/*
+// DeleteImage 根据图片ID删除图片
+func (r *ImageRepo) DeleteImage(imageID int) error {
+	return r.DB.Delete(&model.Image{}, imageID).Error
+}*/
 
-//获取图片
+// GetImagesByBlogID 根据blogid获取图片
+func (r *ImageRepo) GetImagesByBlogID(blogID int64) ([]*model.Image, error) {
+	var images []*model.Image
+	result := r.DB.Where("blog_id = ?", blogID).Find(&images)
+	return images, result.Error
+}
