9592536eede3f64957ea8ea594e2185e87d336a4
Tz <517719033@qq.com>
2025-03-19 23:11:49 +0800

    fix：修改comment表为实现评论的点赞回复功能

--- internal/model/comment.go
+++ internal/model/comment.go
@@ -2,13 +2,15 @@ package model
 
 type Comment struct {
 	CommonModel
-	BlogID       int64  `gorm:"not null" json:"blog_id"`           // 所属帖子ID
-	UserID       int64  `gorm:"not null" json:"user_id"`           // 评论作者ID
-	Content      string `gorm:"type:text;not null" json:"content"` // 评论内容
-	ParentID     int64  `gorm:"default:0" json:"parent_id"`        // 父评论ID（顶级评论，即帖子的直接评论为0）
-	LikesCount   int    `gorm:"default:0" json:"likes_count"`      // 点赞数
-	RepliesCount int    `gorm:"default:0" json:"replies_count"`    // 回复数
+	BlogID       int64     `gorm:"not null" json:"blog_id"`           // 所属帖子 ID
+	UserID       int64     `gorm:"not null" json:"user_id"`           // 评论作者 ID
+	Content      string    `gorm:"type:text;not null" json:"content"` // 评论内容
+	ParentID     int64     `gorm:"default:0" json:"parent_id"`        // 父评论 ID（=0为帖子的直接评论）
+	LikesCount   int       `gorm:"default:0" json:"likes_count"`      // 点赞数
+	RepliesCount int       `gorm:"default:0" json:"replies_count"`    // 回复数
+	Replies      []Comment `gorm:"-" json:"replies"`                  // 回复列表（用于嵌套显示）
 }
+
 type CommentLike struct {
 	CommonModel
 	CommentID int64 `gorm:"not null" json:"comment_id"` // 所属评论ID
