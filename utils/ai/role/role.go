c6ca311c1d7de347b903caa11100177ca12b59d8
Blackcloudss <3444535358@qq.com>
2025-03-21 15:39:51 +0800

    feat:更新AI提示词

---
+++ utils/ai/role/role.go
@@ -0,0 +1,14 @@
+package role
+
+// @Title        role.go
+// @Description
+// @Create       XdpCs 2025-03-21 上午11:58
+// @Update       XdpCs 2025-03-21 上午11:58
+
+// 身份
+const (
+	SYSTEM    = "system"    // 系统
+	USER      = "user"      // 用户
+	ASSISTANT = "assistant" // 助手
+	TOOL      = "tool"      // 工具
+)
