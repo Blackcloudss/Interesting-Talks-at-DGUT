d7a045cc70becf445266abcc002b386eea5a4d08
Tz <517719033@qq.com>
2025-03-24 10:24:09 +0800

    fix:修改json名

--- internal/types/comment.go
+++ internal/types/comment.go
@@ -7,10 +7,10 @@ import (
 
 // CreateCommentReq 创建评论/回复请求结构体
 type CreateCommentReq struct {
-	BlogID       int64  `json:"blog_id"`   // 所属帖子 ID
-	Content      string `json:"content"`   // 评论内容
-	ParentID     int64  `json:"parent_id"` // 父评论 ID（等于0为根评论的回复，不等于0为回复的回复）
-	RootParentID int64  `json:"origin_id"` // 根评论ID（=0为帖子的直接评论——一级评论）
+	BlogID       int64  `json:"blog_id"`        // 所属帖子 ID
+	Content      string `json:"content"`        // 评论内容
+	ParentID     int64  `json:"parent_id"`      // 父评论 ID（等于0为根评论的回复，不等于0为回复的回复）
+	RootParentID int64  `json:"root_parent_id"` // 根评论ID（=0为帖子的直接评论——一级评论）
 }
 
 // CreateCommentResp 创建评论响应结构体
@@ -21,7 +21,7 @@ type CreateCommentResp struct {
 
 // DeleteCommentReq 删除评论请求结构体
 type DeleteCommentReq struct {
-	CommentID int64 `json:"commentID"` // 要删除的评论 ID
+	CommentID int64 `json:"comment_id"` // 要删除的评论 ID
 }
 
 // DeleteCommentResp 删除评论响应结构体
@@ -30,7 +30,7 @@ type DeleteCommentResp struct {
 
 // LikeCommentReq 点赞评论请求结构体
 type LikeCommentReq struct {
-	CommentID int64 `json:"commentID"` // 要点赞的评论 ID
+	CommentID int64 `json:"comment_id"` // 要点赞的评论 ID
 }
 
 // LikeCommentResp 点赞评论响应结构体
@@ -39,7 +39,7 @@ type LikeCommentResp struct {
 
 // UnlikeCommentReq 取消点赞评论请求结构体
 type UnlikeCommentReq struct {
-	CommentID int64 `json:"commentID"` // 要取消点赞的评论 ID
+	CommentID int64 `json:"comment_id"` // 要取消点赞的评论 ID
 }
 
 // UnlikeCommentResp 取消点赞评论响应结构体
