04535a5409d4d3d7b34c8bf6073b97b839738da3
Tz <517719033@qq.com>
2025-03-25 23:31:36 +0800

    fix:修改外键约束

--- internal/model/blog.go
+++ internal/model/blog.go
@@ -1,23 +1,23 @@
 package model
 
-type Blog struct { // 帖子
-	CommonModel           // 删除时间
-	BeLiked        int    `gorm:"column:be_liked;default:0" json:"be_liked"`                                    // 点赞数，默认为0
-	BeCollected    int    `gorm:"column:be_collected;default:0" json:"be_collected"`                            // 收藏数，默认为0
-	CommentCount   int    `gorm:"column:comment_count;default:0" json:"comment_count"`                          // 评论数，默认为0（字段名更清晰）
-	BlogTag        string `gorm:"column:blog_tag;size:30;index" json:"blog_tag"`                                // 帖子分区（主标签）
-	SubTag         string `gorm:"column:sub_tag;size:50;index" json:"sub_tag"`                                  // 子标签
-	ViewPermission string `gorm:"column:view_permission;type:varchar(20);default:'所有人'" json:"view_permission"` // 访问权限，默认为“所有人”
-	UserID         int64  `gorm:"column:user_id" json:"user_id"`                                                // 用户ID
-	Title          string `gorm:"column:title;size:100;not null;index" json:"title"`                            // 标题
-	Content        string `gorm:"column:content;type:text;not null" json:"content"`                             // 内容，不可为空
-	// 关联一级评论
-	FirstComments []FirstComment `gorm:"foreignKey:blog_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	// 关联二级评论
-	SecondComments []SecondComment `gorm:"foreignKey:blog_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+type Blog struct {
+	CommonModel
+	BeLiked        int             `gorm:"column:be_liked;default:0" json:"be_liked"`
+	BeCollected    int             `gorm:"column:be_collected;default:0" json:"be_collected"`
+	CommentCount   int             `gorm:"column:comment_count;default:0" json:"comment_count"`
+	BlogTag        string          `gorm:"column:blog_tag;size:30;index" json:"blog_tag"`
+	SubTag         string          `gorm:"column:sub_tag;size:50;index" json:"sub_tag"`
+	ViewPermission string          `gorm:"column:view_permission;type:varchar(20);default:'所有人'" json:"view_permission"`
+	UserID         int64           `gorm:"column:user_id" json:"user_id"`
+	Title          string          `gorm:"column:title;size:100;not null;index" json:"title"`
+	Content        string          `gorm:"column:content;type:text;not null" json:"content"`
+	FirstComments  []FirstComment  `gorm:"foreignKey:BlogID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKeyName:fk_blog_first_comments"`
+	SecondComments []SecondComment `gorm:"foreignKey:BlogID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKeyName:fk_blog_second_comments"`
 }
 
-func (b *Blog) TableName() string { return "blog" }
+func (t *Blog) TableName() string {
+	return "blog"
+}
 
 type Like struct { // 点赞
 	CommonModel
--- internal/model/comment.go
+++ internal/model/comment.go
@@ -3,31 +3,26 @@ package model
 // 使用两层型
 type FirstComment struct {
 	CommonModel
-	BlogID       int64  `gorm:"column:blog_id;not null;constraint:fk_blog_first_comments:blog_id REFERENCES blog(id) ON DELETE CASCADE ON UPDATE CASCADE" json:"blog_id"` // 所属帖子 ID
-	UserID       int64  `gorm:"column:user_id;not null" json:"user_id"`                                                                                                   // 评论者 ID
-	Content      string `gorm:"column:content;type:text;not null" json:"content"`                                                                                         // 评论内容
-	LikesCount   int    `gorm:"column:likes_count;default:0" json:"likes_count"`                                                                                          // 点赞数
-	RepliesCount int    `gorm:"column:replies_count;default:0" json:"replies_count"`                                                                                      // 回复数
-	// 关联二级评论
-	SecondComments []SecondComment `gorm:"foreignKey:root_parent_id;references:id;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	BlogID         int64           `gorm:"column:blog_id;not null;constraint:fk_blog_first_comment:blog_id REFERENCES blog(id) ON DELETE CASCADE ON UPDATE CASCADE" json:"blog_id"`
+	UserID         int64           `gorm:"column:user_id;not null" json:"user_id"`
+	Content        string          `gorm:"column:content;type:text;not null" json:"content"`
+	LikesCount     int             `gorm:"column:likes_count;default:0" json:"likes_count"`
+	RepliesCount   int             `gorm:"column:replies_count;default:0" json:"replies_count"`
+	SecondComments []SecondComment `gorm:"foreignKey:RootParentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKeyName:fk_first_comment_second_comments"`
 }
 
 func (t *FirstComment) TableName() string {
 	return "first_comment"
 }
 
-/*
-对一级评论进行回复的二级评论
-对二级评论进行回复的二级评论
-*/
 type SecondComment struct {
 	CommonModel
-	BlogID       int64  `gorm:"column:blog_id;not null;constraint:fk_blog_second_comments:blog_id REFERENCES blog(id) ON DELETE CASCADE ON UPDATE CASCADE" json:"blog_id"` // 所属帖子 ID
-	UserID       int64  `gorm:"column:user_id;not null" json:"user_id"`                                                                                                    // 评论者 ID
-	Content      string `gorm:"column:content;type:text;not null" json:"content"`                                                                                          // 评论内容
-	ParentID     int64  `gorm:"column:parent_id;default:0" json:"parent_id"`                                                                                               // 父评论ID
-	RootParentID int64  `gorm:"column:root_parent_id;default:0" json:"root_parent_id"`                                                                                     // 根评论ID
-	RepliesCount int    `gorm:"column:replies_count;default:0" json:"replies_count"`                                                                                       // 回复数
+	BlogID       int64  `gorm:"column:blog_id;not null;constraint:fk_blog_second_comment:blog_id REFERENCES blog(id) ON DELETE CASCADE ON UPDATE CASCADE" json:"blog_id"`
+	UserID       int64  `gorm:"column:user_id;not null" json:"user_id"`
+	Content      string `gorm:"column:content;type:text;not null" json:"content"`
+	ParentID     int64  `gorm:"column:parent_id;default:0" json:"parent_id"`
+	RootParentID int64  `gorm:"column:root_parent_id;default:0" json:"root_parent_id"`
+	RepliesCount int    `gorm:"column:replies_count;default:0" json:"replies_count"`
 }
 
 func (t *SecondComment) TableName() string {
