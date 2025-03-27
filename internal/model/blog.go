2e217e028aecc3427111850e669d6901fd0c99e3
Tz <517719033@qq.com>
2025-03-27 23:58:15 +0800

    fix:命名统一

--- internal/model/blog.go
+++ internal/model/blog.go
@@ -2,8 +2,8 @@ package model
 
 type Blog struct {
 	CommonModel
-	LikeCount      int    `gorm:"column:be_liked;default:0" json:"like_count"`
-	CollectCount   int    `gorm:"column:be_collected;default:0" json:"collect_count"`
+	LikeCount      int    `gorm:"column:like_count;default:0" json:"like_count"`
+	CollectCount   int    `gorm:"column:collect_count;default:0" json:"collect_count"`
 	CommentCount   int    `gorm:"column:comment_count;default:0" json:"comment_count"`
 	BlogTag        string `gorm:"column:blog_tag;size:30;index" json:"blog_tag"`
 	SubTag         string `gorm:"column:sub_tag;size:50;index" json:"sub_tag"`
