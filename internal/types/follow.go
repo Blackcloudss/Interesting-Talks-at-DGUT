7b44b2cb3f43ef2270f899118e0578b0660bd1fd
Tz <517719033@qq.com>
2025-03-28 15:55:47 +0800

    fix:修改关注操作请求响应体

--- internal/types/follow.go
+++ internal/types/follow.go
@@ -7,24 +7,16 @@ type FollowReq struct {
 
 // FollowResp 关注响应结构体
 type FollowResp struct {
-}
-
-// UnfollowReq 取消关注请求结构体
-type UnfollowReq struct {
-	FollowedID int64 `json:"followed_id"` // 被关注者ID
-}
-
-// UnfollowResp 取消关注响应结构体
-type UnfollowResp struct {
+	IsFollowing bool `json:"is_following"` // 当前用户是否关注了该用户
 }
 
 // FollowInfo 关注的用户或粉丝通用信息
 type FollowInfo struct {
-	UserID       int64  `json:"user_id"`        // 用户ID
-	Nickname     string `json:"nickname"`       // 昵称
-	Avatar       string `json:"avatar"`         // 头像
-	FollowedAt   string `json:"followed_At"`    // 已关注的时间
-	IsFollowedBy bool   `json:"is_followed_by"` // 当前用户是否关注了该用户
+	UserID      int64  `json:"user_id"`      // 用户ID
+	Nickname    string `json:"nickname"`     // 昵称
+	Avatar      string `json:"avatar"`       // 头像
+	FollowedAt  string `json:"followed_At"`  // 已关注的时间
+	IsFollowing bool   `json:"is_following"` // 当前用户是否关注了该用户
 }
 
 // GetFollowingsReq 获取我的关注列表请求结构体
